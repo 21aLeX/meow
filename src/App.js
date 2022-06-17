3cd1d1fabaf8c0ccc2b5f0a5ff0b502a402fc7aa
21aLeX <aleksandra12mail@mail.ru>
2022-06-17 16:13:34 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 
 function App() {
+
+  const story = <div className='story greyTitle'>Сказочное заморское явство</div>
+  const title = <div className='title'>Нямушка</div>
+
   return (
     <div className="App">
       <div className='container'>
-      <div className='box'></div>
-      <div className='box'></div>
-      <div className='box'></div>
+        <div className='box'>
+          {story}
+          {title}
+          <div className='food'>с фуа-гра</div>
+          <div className='mouse greyTitle'>10 порций мышь 
+          <br/>
+          в подарок</div>
+          <div className='img'></div>
+        </div>
+        <div className='box'>
+          {story}
+          {title}
+          <div className='food'>с рыбой</div>
+        </div>
+        <div className='box'>
+          {story}
+          {title}
+          <div className='food'>с курой</div>
+        </div>
       </div>
     </div>
   );
