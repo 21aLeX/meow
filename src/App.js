e7252a7ba386d16f86710a6d2d2e217ad913633b
21aLeX <aleksandra12mail@mail.ru>
2022-06-20 17:33:24 +0800

    new

--- src/App.js
+++ src/App.js
@@ -12,20 +12,40 @@ function App() {
           {story}
           {title}
           <div className='food'>с фуа-гра</div>
-          <div className='mouse greyTitle'>10 порций мышь 
+          <div className='mouse greyTitle'>10 порций
           <br/>
-          в подарок</div>
+          мышь в подарок</div>
+          <div className='boxImg'>
+
           <div className='img'></div>
+          </div>
         </div>
         <div className='box'>
           {story}
           {title}
           <div className='food'>с рыбой</div>
+          <div className='mouse greyTitle'>40 порций
+          <br/>
+         2 мыши в подарок</div>
+          <div className='boxImg'>
+
+          <div className='img'></div>
+          </div>
         </div>
         <div className='box'>
           {story}
           {title}
           <div className='food'>с курой</div>
+          <div className='mouse greyTitle'>100 порций
+          <br/>
+         5 мышей в подарок
+          <br/>
+          заказчик доволен
+          </div>
+          <div className='boxImg'>
+
+          <div className='img'></div>
+          </div>
         </div>
       </div>
     </div>
