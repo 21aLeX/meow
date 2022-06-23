8d3d1b96dfffc96b80967a7378594b7ca15bd268
21aLeX <aleksandra12mail@mail.ru>
2022-06-23 14:23:35 +0800

    new

--- src/App.js
+++ src/App.js
@@ -8,72 +8,80 @@ function App() {
 
   return (
     <div className="App">
-    <div className='catContainer'>
-      Ты сегодня покормил кота?
+      <div className='catContainer'>
+        Ты сегодня покормил кота?
       </div>
       <div className='container'>
         <div className='bigBox'>
-        <div className='box'>
-          {story}
-          {title}
-          <div className='food'>с фуа-гра</div>
-          <div className='mouse greyTitle'>10 порций
-            <br />
-            мышь в подарок</div>
+          <div className='borderBox'>
+            <div className='box'>
+              {story}
+              {title}
+              <div className='food'>с фуа-гра</div>
+              <div className='mouse greyTitle'>10 порций
+                <br />
+                мышь в подарок</div>
 
-          <div className='img'>
-            <div className='circle'>
-            <div className='textKg'>0,5 
-            <div className='Kg'>кг</div></div>
+              <div className='img'>
+                <div className='circle'>
+                  <div className='textKg'>0,5
+                    <div className='Kg'>кг</div></div>
+                </div>
+              </div>
             </div>
           </div>
+          <div className='happy'>Чего сидишь? Порадуй котэ, <a href='1'>купи</a>
+            <div className='point'>.</div></div>
         </div>
-        <div className='happy'>Чего сидишь? Порадуй котэ, <a href='1'>купи</a>
-        <div className='point'>.</div></div>
-      </div>
         <div className='bigBox'>
-        <div className='box'>
-          {story}
-          {title}
-          <div className='food'>с рыбой</div>
-          <div className='mouse greyTitle'>40 порций
-            <br />
-            2 мыши в подарок</div>
+          <div className='borderBox'>
+            <div className='box'>
+              {story}
+              {title}
+              <div className='food'>с рыбой</div>
+              <div className='mouse greyTitle'>40 порций
+                <br />
+                2 мыши в подарок</div>
 
-          <div className='img'>
-            <div className='circle'>
-            <div className='textKg'>2             
-            <br />
-            <div className='Kg'>кг</div></div>
+              <div className='img'>
+                <div className='circle'>
+                  <div className='textKg'>2
+                    <br />
+                    <div className='Kg'>кг</div></div>
+                </div>
+              </div>
             </div>
           </div>
+          <div className='happy'>Чего сидишь? Порадуй котэ, <a href='1'>купи</a>
+            <div className='point'>.</div></div>
         </div>
-        <div className='happy'>Чего сидишь? Порадуй котэ, <a href='1'>купи</a>
-        <div className='point'>.</div></div>
-      </div>
         <div className='bigBox'>
-        <div className='box'>
-          {story}
-          {title}
-          <div className='food'>с курой</div>
-          <div className='mouse greyTitle'>100 порций
-            <br />
-            5 мышей в подарок
-            <br />
-            заказчик доволен
-          </div>
+          <div className='shadovBox'>
+          <div className='borderBox'>
+            <div className='box'>
+              {story}
+              {title}
+              <div className='food'>с курой</div>
+              <div className='mouse greyTitle'>100 порций
+                <br />
+                5 мышей в подарок
+                <br />
+                заказчик доволен
+              </div>
 
-          <div className='img'>
-            <div className='circle'>
-            <div className='textKg'>5
-            <br />
-            <div className='Kg'>кг</div></div>
+              <div className='img'>
+                <div className='circle'>
+                  <div className='textKg'>5
+                    <br />
+                    <div className='Kg'>кг</div></div>
+                </div>
+              </div>
             </div>
           </div>
+          </div>
+          <div className='happy'>Чего сидишь? Порадуй котэ, <a href='1'>купи</a>
+            <div className='point'>.</div></div>
         </div>
-        <div className='happy'>Чего сидишь? Порадуй котэ, <a href='1'>купи</a>
-        <div className='point'>.</div></div>
-      </div>
       </div>
     </div>
   );
